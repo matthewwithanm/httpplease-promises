75c1d41a4c097dc328b5620148a7157790f88567
Matthew Dapena-Tretter <m@tthewwithanm.com>
2014-05-04 21:43:01 -0400

    Remove unneeded endpoint

--- test/server.js
+++ test/server.js
@@ -10,11 +10,6 @@ app.get('/getjson', cors(), function (req, res) {
     res.json({hello: 'world'});
 });
 
-// Echo the headers back.
-app.all('/headers', cors(), function (req, res) {
-    res.json(req.headers);
-});
-
 app.all('/404', cors(), function (req, res) {
     res.json(404, {sad: 'panda'});
 });
