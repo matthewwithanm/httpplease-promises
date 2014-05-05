45845b203fa950df288f0dec956e29aa13502dd0
Matthew Dapena-Tretter <m@tthewwithanm.com>
2014-05-04 21:40:30 -0400

    Remove browser build stuff for now

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,30 +4,6 @@ module.exports = function(grunt) {
     // Project configuration
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        browserify: {
-            standalone: {
-                options: {
-                    bundleOptions: {
-                        standalone: 'httppleasepromises'
-                    }
-                },
-                files: {
-                    './browser-builds/standalone/httpplease-promises.js': './lib/index.js'
-                }
-            },
-        },
-        uglify: {
-            browserbuilds: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: './browser-builds/',
-                        src: '**/*.js',
-                        dest: './browser-builds/'
-                    }
-                ]
-            }
-        },
         express: {
             testserver: {
                 options: {
@@ -72,13 +48,8 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-mocha-test');
     grunt.loadNpmTasks('grunt-bump');
-    grunt.loadNpmTasks('grunt-browserify');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     // Define tasks.
-    grunt.registerTask('build', ['build:standalone']);
-    grunt.registerTask('build:standalone', ['browserify', 'uglify:browserbuilds']);
-    grunt.registerTask('default', ['build']);
     grunt.registerTask('test', ['runtestserver', 'mochaTest']);
     grunt.registerTask('runtestserver', ['express:testserver']);
 };
