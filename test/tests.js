4f0d6cdfd2664312a501237286884edc51d8aa04
Matthew Dapena-Tretter <m@tthewwithanm.com>
2014-05-04 21:43:20 -0400

    Add `then` and `catch` to the request object

--- test/tests.js
+++ test/tests.js
@@ -3,13 +3,47 @@
 
 var
     assert = require('chai').assert,
-    http = require('httpplease'),
+    httpplease = require('httpplease'),
     promises = require('../lib/index'),
-    testServerUrl = 'http://localhost:4001';
+    testServerUrl = 'http://localhost:4001',
+    http = httpplease.use(promises);
 
 
 describe('httpplease-promises', function () {
-    it('passes a test', function () {
-        assert(true);
+    it('makes requests thenable', function (done) {
+        http
+            .get(testServerUrl + '/getjson')
+            .then(function (res) {
+                assert.equal(res.text, JSON.stringify({hello: 'world'}));
+                done();
+            });
+    });
+    it('still calls onload', function (done) {
+        http.get({
+            url: testServerUrl + '/getjson',
+            onload: function (res) {
+                assert.equal(res.text, JSON.stringify({hello: 'world'}));
+                done();
+            }
+        });
+    });
+    it('makes requests catchable', function (done) {
+        http
+            .get(testServerUrl + '/404')
+            .catch(function (err) { // jshint ignore:line
+                assert.equal(err.status, 404);
+                assert(err.isHttpError);
+                done();
+            });
+    });
+    it('still calls onerror', function (done) {
+        http.get({
+            url: testServerUrl + '/404',
+            onerror: function (err) {
+                assert.equal(err.status, 404);
+                assert(err.isHttpError);
+                done();
+            }
+        });
     });
 });
